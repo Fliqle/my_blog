a15f46a98d98ffb7faf4aa13a53ea2b000acfc8a
fliqle <sirotkin.lab@gmail.com>
2019-08-21 19:55:09 +0300

    new logo

---
+++ src/@narative/gatsby-theme-novela/components/Logo/logo.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import mediaqueries from "@narative/gatsby-theme-novela/src/styles/media";
+
+const Logo = ({ fill = "#fff" }: { fill?: string }) => {
+  return (
+    <LogoContainer>
+<h1> Valeriy Sirotkin</h1>
+    </LogoContainer>
+  );
+};
+
+export default Logo;
+
+const LogoContainer = styled.div`
+  .Logo__Mobile {
+    display: none;
+  }
+
+  ${mediaqueries.tablet`
+    .Logo__Desktop {
+      display: none;
+    }
+
+    .Logo__Mobile{
+      display: block;
+    }
+  `}
+`;
